60cc26d3a0a2f54e7a46bbf17c5ec7015be83b43
kudinartem <artemkudin1994@gmail.com>
2022-07-11 15:36:31 +0300

    HW_3

---
+++ main.js
@@ -0,0 +1,17 @@
+// Task No1
+
+let fibonacci = parseInt(prompt('Enter a number (Fibonacci sequence):'));
+let fibConst = [0, 1];
+for (i = 2; i < fibonacci; i++) {
+   fibConst[i] = fibConst[i - 1] + fibConst[i - 2];
+};
+console.log(fibConst.join(', '));
+
+//Task No2
+
+let NumberladderRange = parseInt(prompt('Enter a number (number ladder range):'));
+let ladderNumberEntry = [];
+for (j = 1; j < NumberladderRange + 1; j++) {
+   ladderNumberEntry.push(j);
+   console.log(ladderNumberEntry.join(' ')); 
+};
